5e5b446945a89d6ee34020e99489b2bd047ec39a
Shriya Gakkhar <161853135+shriya-gakkhar1@users.noreply.github.com>
2025-11-19 17:38:23 +0530

    Delete task1.py

--- task1.py
+++
@@ -1,6 +0,0 @@
-num=int(input("enter the number:))
-if(num%2==n):
-              print(num," is even an even number")
-              else:
-              print(num,"is an odd number)
-              
